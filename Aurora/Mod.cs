a6dc878ad8e5b07ab0b734a608cf63df8130e8c4
ZxnoVRC <76539294+ZxnoVRC@users.noreply.github.com>
2022-07-11 17:33:41 -0400

    i hate github

--- Aurora/Mod.cs
+++ Aurora/Mod.cs
@@ -14,7 +14,10 @@ namespace Aurora
     {
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
-            MelonCoroutines.Start(changename());
+            //  Action changename = new Action(changename);
+            //Parallel.Invoke(Changename);
+            //Task.Run(Changename);
+            MelonCoroutines.Start(Changename());
         }
 
 
@@ -23,10 +26,10 @@ namespace Aurora
 
 
 
-      
+
 
         public static bool nametest = true;
-        public static IEnumerator changename()
+        public static IEnumerator Changename()
         {
             string GettLine(string fileName, int line)
             {
@@ -40,52 +43,51 @@ namespace Aurora
 
             //dont bully me i know the code is dogshit and unoptimized but it still works fine without fps drops and i am working on other shit rn :(
 
-            if (GettLine("Mods/config.txt", 1) == "0")
+            
+            if (GettLine("Mods/config.json", 1) == "2")
             {
-                if (GettLine("Mods/config.txt", 1) == "1")
+                while (nametest)
                 {
-                    Console.WriteLine(GettLine("Mods/config.txt", 1) + " 31232");
-                    while (nametest)
+                    yield return new WaitForEndOfFrame();
+                    try
                     {
-
-                        yield return new WaitForEndOfFrame();
-                        try
+                        if (APIUser.CurrentUser._displayName_k__BackingField != RoomManager.field_Internal_Static_ApiWorld_0.authorName)
                         {
-                            if (APIUser.CurrentUser._displayName_k__BackingField != RoomManager.field_Internal_Static_ApiWorld_0.authorName)
+                            if (RoomManager.field_Internal_Static_ApiWorld_0.authorId != "8JoV9XEdpo")
                             {
-                                if (RoomManager.field_Internal_Static_ApiWorld_0.authorId != "8JoV9XEdpo")
-                                {
 
-                                    APIUser.CurrentUser._displayName_k__BackingField = RoomManager.field_Internal_Static_ApiWorld_0.authorName;
-                                    APIUser.CurrentUser._id_k__BackingField = RoomManager.field_Internal_Static_ApiWorld_0.authorId;
-                                }
-                            }
-
-
-                        }
-                        catch { }
-                    }
-                }
-                else if (GettLine("Mods/config.txt", 1) == "2")
-                {
-                    while (nametest)
-                    {
-                        yield return new WaitForEndOfFrame();
-                        try
-                        {
-                            if (APIUser.CurrentUser._displayName_k__BackingField != RoomManager.field_Internal_Static_ApiWorld_0.authorName)
-                            {
-                                if (RoomManager.field_Internal_Static_ApiWorld_0.authorId != "8JoV9XEdpo")
-                                {
-
-                                    APIUser.CurrentUser._displayName_k__BackingField = GettLine("Mods/config.txt", 2);
-                                    APIUser.CurrentUser._id_k__BackingField = GettLine("Mods/config.txt", 3);
-                                }
+                                APIUser.CurrentUser._displayName_k__BackingField = GettLine("Mods/config.txt", 2);
+                                APIUser.CurrentUser._id_k__BackingField = GettLine("Mods/config.txt", 3);
                             }
                         }
-                        catch { }
                     }
+                    catch { }
                 }
+            }
+            else if (GettLine("Mods/config.txt", 1) == "1")
+            {
+                Console.WriteLine(GettLine("Mods/config.txt", 1) + " 31232");
+                while (nametest)
+                {
+
+                    yield return new WaitForEndOfFrame();
+                    try
+                    {
+                        if (APIUser.CurrentUser._displayName_k__BackingField != RoomManager.field_Internal_Static_ApiWorld_0.authorName)
+                        {
+                            if (RoomManager.field_Internal_Static_ApiWorld_0.authorId != "8JoV9XEdpo")
+                            {
+
+                                APIUser.CurrentUser._displayName_k__BackingField = RoomManager.field_Internal_Static_ApiWorld_0.authorName;
+                                APIUser.CurrentUser._id_k__BackingField = RoomManager.field_Internal_Static_ApiWorld_0.authorId;
+                            }
+                        }
+
+
+                    }
+                    catch { }
+                }
+                //return null;
             }
         }
 
